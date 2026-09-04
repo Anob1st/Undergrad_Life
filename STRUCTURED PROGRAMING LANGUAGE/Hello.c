113a19b1df226ec6c3478f22aa0383df996ced90
Anob1st <227586548+Anob1st@users.noreply.github.com>
2026-09-04 12:23:42 +0600

    Add Hello.c for greeting user in C

---
+++ STRUCTURED PROGRAMING LANGUAGE/Hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char name[100];
+    scanf("%s", name);
+
+    // Print the greeting lines
+    printf("Hello, %s!\n", name);
+    printf("Welcome to C programming!");
+
+    return 0;
+}
