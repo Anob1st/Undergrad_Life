5c6117582b010fe65bd6f904dc7991a27ff45d5d
Anob1st <227586548+Anob1st@users.noreply.github.com>
2026-09-04 12:36:12 +0600

    Create Shopping receipt.c

---
+++ STRUCTURED PROGRAMING LANGUAGE/Shopping receipt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char item[100];
+    double price;
+    int quantity;
+    scanf("%s", item);
+    scanf("%lf", &price);
+    scanf("%d", &quantity);
+    double total = price * quantity;
+    printf("Item: %s\n", item);
+    printf("Price: $%.2f\n", price);
+    printf("Quantity: %d\n", quantity);
+    printf("Total: $%.2f\n", total);
+    return 0;
+}
+
