b399080d4a74c64daf09de304cb8e371be22dd64
Anob1st <227586548+Anob1st@users.noreply.github.com>
2026-09-07 19:06:19 +0600

    Create Different data type.c

---
+++ STRUCTURED PROGRAMING LANGUAGE/variables and data types/Different data type.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age = 18;
+    float height = 5.8;
+    double pi = 3.141592;
+    char grade = 'A';
+
+    printf("%d\n", age);
+    printf("%.1f\n", height);
+    printf("%lf\n", pi);
+    printf("%c\n", grade);
+
+    return 0;
+}
