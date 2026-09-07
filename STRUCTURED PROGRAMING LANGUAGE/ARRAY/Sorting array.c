2638f0c3fcc55770794323720237fb8b9610fab9
Anob1st <227586548+Anob1st@users.noreply.github.com>
2026-09-07 21:52:19 +0600

    Add sorting algorithm for an integer array

---
+++ STRUCTURED PROGRAMING LANGUAGE/ARRAY/Sorting array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5];
+    int temp;
+    for(int i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);}
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = i + 1; j < 5; j++)
+        {
+            if(a[i] > a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+            }
+            }
+
+    printf("Sorted array:\n");
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
