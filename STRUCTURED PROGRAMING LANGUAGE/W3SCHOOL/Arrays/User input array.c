98ce56933bc87d665184da600a32de61c61bda26
Anob1st <227586548+Anob1st@users.noreply.github.com>
2026-09-24 19:28:36 +0600

    Fix formatting issues in User input array.c

---
+++ STRUCTURED PROGRAMING LANGUAGE/W3SCHOOL/Arrays/User input array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[5];
+    for (int i = 0; i < 5; i++)
+        scanf("%d", &numbers[i]);
+
+
+
+
+
+  
+    for (int i = 0; i < 5; i++)
+        printf("%d ", numbers[i]);
+    return 0;
+}
